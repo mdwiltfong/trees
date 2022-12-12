41c29e0def2d4874b2071060a6c38350b9ba569e
mdwiltfong <wiltfong.michael1@gmail.com>
2022-12-11 23:49:37 -0500

    min depth function

--- dsa-trees/binary-tree.js
+++ dsa-trees/binary-tree.js
@@ -17,59 +17,57 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
-
+    if (!this.root) return 0;
+    function minDepthHelper(node, depth) {
+      if (node.left === null && node.right === null) return 1;
+      if (node.left == null) minDepthHelper(node.right, depth + 1);
+      if (node.right == null) minDepthHelper(node.left, depth + 1);
+      return (
+        Math.min(
+          minDepthHelper(node.left, depth),
+          minDepthHelper(node.right, depth)
+        ) + 1
+      );
+    }
+    return minDepthHelper(this.root, 0);
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
-  maxDepth() {
-
-  }
+  maxDepth() {}
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
-  maxSum() {
-
-  }
+  maxSum() {}
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
-  nextLarger(lowerBound) {
-
-  }
+  nextLarger(lowerBound) {}
 
   /** Further study!
    * areCousins(node1, node2): determine whether two nodes are cousins
    * (i.e. are at the same level but have different parents. ) */
 
-  areCousins(node1, node2) {
-
-  }
+  areCousins(node1, node2) {}
 
   /** Further study!
    * serialize(tree): serialize the BinaryTree object tree into a string. */
 
-  static serialize() {
-
-  }
+  static serialize() {}
 
   /** Further study!
    * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
 
-  static deserialize() {
-
-  }
+  static deserialize() {}
 
   /** Further study!
    * lowestCommonAncestor(node1, node2): find the lowest common ancestor
    * of two nodes in a binary tree. */
 
-  lowestCommonAncestor(node1, node2) {
-    
-  }
+  lowestCommonAncestor(node1, node2) {}
 }
 
 module.exports = { BinaryTree, BinaryTreeNode };
